8ef5b5e621f6d510c1fc53930ce0749dc362cd5b
bonk-dev <github.p0j6t@passinbox.com>
2025-04-30 22:20:50 +0200

    Add Docusaurus files

---
+++ docusaurus.config.ts
@@ -0,0 +1,80 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Infpot',
+  tagline: 'Notatki z informatyki na MSie',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://dpago.dev',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'bonk-dev', // Usually your GitHub org/user name.
+  projectName: 'infpot', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'pl',
+    locales: ['pl'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          path: 'notatki',
+          routeBasePath: 'notatki'
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/infpot-social-card.jpg',
+    navbar: {
+      title: 'Infpot',
+      logo: {
+        alt: 'Infpot Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Notatki',
+        },
+        {
+          href: 'https://github.com/bonk-dev/infpot',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,9 @@
+import type {ReactNode} from 'react';
+import styles from './styles.module.css';
+
+export default function HomepageFeatures(): ReactNode {
+  return (
+    <section className={styles.features}>
+    </section>
+  );
+}
