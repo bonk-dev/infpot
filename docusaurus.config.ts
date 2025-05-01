002c800121955e0ae8e6bf57411383211db2e18c
bonk-dev <github.p0j6t@passinbox.com>
2025-05-01 20:06:46 +0200

    Set the correct baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: 'https://dpago.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/infpot/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
